bf32cd3611a7ae07f2a5a97044103d657b8b2e31
Roberto Melo <rmello.filho@hotmail.com>
2023-05-25 20:49:16 -0300

    Aula 24 Trabalhando com Dados #2

--- dados_2.js
+++ dados_2.js
@@ -1,2 +1,13 @@
+console.log(19.9 * 0.6);
+
 let preco = 19.90;
 let desconto = 0.4;
+let precoComDesconto = preco * (1 - desconto);
+console.log(precoComDesconto);
+
+let nome = "Caderno"; // string é uma sequência de caracteres podendo ter números, símbolos, letras e espaços
+let categoria = "Papelaria";
+console.log("Produto: " + nom
+            + ", Categoria: " + categoria
+            + ", Preço: " + preco
+            + ", Desconto: " + desconto);
